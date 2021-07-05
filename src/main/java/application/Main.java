9a8492486974a896717818ae5389252036686d38
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-05 10:19:47 -0300

    Testando exception

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -9,8 +9,9 @@ import model.Pessoa;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Pessoa pessoa = null;
 
         try {
             System.out.print("Digite o nome da pessoa: ");
@@ -19,25 +20,33 @@ public class Main {
             String telefone = sc.nextLine();
             System.out.print("Digite o tipo de conta: Corrente ou Poupanca");
             String tipo = sc.next();
-            if (tipo == "Corrente") {
+            if (tipo.equals("Corrente")) {
                 System.out.print("Digite o numero da conta: ");
                 int numero = sc.nextInt();
                 System.out.print("Digite o saldo inicial: ");
                 double saldo = sc.nextDouble();
                 System.out.print("Digite o limite da conta");
                 double limite = sc.nextDouble();
-                Pessoa pessoa = new Pessoa(nome, telefone, new ContaCorrente(numero, nome, saldo, limite));
-            } else if (tipo == "Poupanca") {
+                pessoa = new Pessoa(nome, telefone, new ContaCorrente(numero, nome, saldo, limite));
+            } else if (tipo.equals("Poupanca")) {
                 System.out.print("Digite o numero da conta: ");
                 int numero = sc.nextInt();
                 System.out.print("Digite o saldo inicial: ");
                 double saldo = sc.nextDouble();
                 System.out.print("Digite o rendimento");
                 double rendimento = sc.nextDouble();
-                Pessoa pessoa = new Pessoa(nome, telefone, new ContaPoupanca(numero, nome, saldo, rendimento));
+                pessoa = new Pessoa(nome, telefone, new ContaPoupanca(numero, nome, saldo, rendimento));
             }
-        } catch (SaqueException e) {
 
+            System.out.print("Digite quanto você quer sacar da conta: ");
+            double qtsSacar = sc.nextDouble();
+            //Testando tratamento de excecao para saque
+            pessoa.getConta().saque(qtsSacar);
+
+        } catch (SaqueException e) {
+            System.out.println("Erro ao sacar: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Erro inesperado: " + e.getMessage());
         } finally {
             sc.close();
         }
